23317b2dae59aea2ca7aff6e9d50a2030a4e136b
olleh-dlrow <1136246408@qq.com>
2020-07-11 08:39:18 +0800

    nothing

---
+++ football/common1/send_chat.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: send_chat.h
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 10 Jul 2020 11:58:16 PM CST
+ ************************************************************************/
+
+#ifndef _SEND_CHAT_H
+#define _SEND_CHAT_H
+
+void send_chat();
+
+#endif
